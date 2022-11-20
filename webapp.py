a46213f1fcbcc08295e9753623aae4e589ecf9e5
MateuszSzybiak <matszybiak@gmail.com>
2022-11-20 19:47:12 +0100

    Add basic project files

---
+++ webapp.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p> Hello World!</p>"
+
+
+@app.route("/info")
+def display_info():
+    return "This subpage is additional info"
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
